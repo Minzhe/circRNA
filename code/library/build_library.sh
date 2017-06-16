b7763d0f0ba24eddf9c02be5a1f4ce521d40c4ed
Minzhe Zhang <zenroute.mzhang@gmail.com>
2017-06-16 09:38:57 -0500

    modify build_library.sh

--- code/library/build_library.sh
+++ code/library/build_library.sh
@@ -15,7 +15,7 @@ cd ~/projects/circRNA/data/library/dm3
 perl ~/projects/Clirc/bin/circRNA2region.pl 50 dm3_circRNA_coordinates.txt dm3_CIRI.txt dm3_PMID25242144.txt dm3_PMID25544350.txt
 perl ~/projects/Clirc/bin/index.pl ~/data/genomes/dm3/dm3_masked.fa dm3_index.txt 
 perl ~/projects/Clirc/bin/query.pl dm3_index.txt ~/data/genomes/dm3/dm3_masked.fa dm3_circRNA_coordinates.txt dm3_halves.txt 1
-perl ~/projects/Clirc/bin/half2library.pl dm3_halves.txt dm3_circRNA_large.fa
+perl ~/projects/Clirc/bin/half2library.pl dm3_halves.txt dm3_circRNA.fa
 perl ~/projects/Clirc/bin/query.pl dm3_index.txt ~/data/genomes/dm3/dm3_masked.fa dm3_circRNA_coordinates.txt_on_genome dm3_circRNA_on_genome.fa 1 # change this
 
 rm -f -r gsnap
@@ -44,14 +44,14 @@ gmap_build --build-sarray=0 -D gsnap -d circRNA_on_genome hg19_circRNA_on_genome
 cd ~/projects/circRNA/data/library/mm9
 
 perl ~/projects/Clirc/bin/circRNA2region.pl 50 mm9_circRNA_coordinates.txt mm9_circBase.txt mm9_CIRI.txt mm9_PMID25921068.txt
-#perl ~/software/packages/Motif/index.pl ~/data/genomes/mm9/mm9_masked.fa mm9_index.txt
-#perl ~/software/packages/Motif/query.pl mm9_index.txt ~/data/genomes/mm9/mm9_masked.fa mm9_circRNA_coordinates.txt mm9_halves.txt 1
-#perl ~/iproject/circRNA/code/library/half2library.pl mm9_halves.txt mm9_circRNA_large.fa
+perl ~/projects/Clirc/bin/index.pl ~/data/genomes/mm9/mm9_masked.fa mm9_index.txt
+perl ~/projects/Clirc/bin/query.pl mm9_index.txt ~/data/genomes/mm9/mm9_masked.fa mm9_circRNA_coordinates.txt mm9_halves.txt 1
+perl ~/projects/Clirc/bin/half2library.pl mm9_halves.txt mm9_circRNA.fa
 perl ~/projects/Clirc/bin/query.pl mm9_index.txt ~/data/genomes/mm9/mm9_masked.fa mm9_circRNA_coordinates.txt_on_genome mm9_circRNA_on_genome.fa 1 # change this
 
-#rm -f -r gsnap
-#mkdir gsnap
-#gmap_build -D gsnap -d circRNA ~/data/genomes/mm9/mm9_masked.fa mm9_circRNA.fa
-#gmap_build -D gsnap -d genome ~/data/genomes/mm9/mm9_masked.fa
+rm -f -r gsnap
+mkdir gsnap
+gmap_build -D gsnap -d circRNA ~/data/genomes/mm9/mm9_masked.fa mm9_circRNA.fa
+gmap_build -D gsnap -d genome ~/data/genomes/mm9/mm9_masked.fa
 gmap_build --build-sarray=0 -D gsnap -d circRNA_on_genome mm9_circRNA_on_genome.fa
 
